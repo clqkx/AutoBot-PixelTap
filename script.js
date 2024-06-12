611a47c4db5f137388ae86632d8f965cc1dd6740
clqkx <clqkx@proton.me>
2024-06-12 08:26:44 +0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log(`${consoleStyles.prefix}🚀 Injecting...`, consoleStyles.green);
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 const stats = { victories: 0, defeats: 0, totalCoins: 0 };
-let isBattle = false;
+let isBattle, isFinish = false;
 
 const checkState = async () => {
     try {
@@ -14,7 +14,7 @@ const checkState = async () => {
             return await sleep(1000), await checkState();
         } else if (document.querySelector('._battle_zmmvw_1') && !isBattle) {
             return isBattle = true, await playBattle();
-        } else if (document.querySelector('._resultContainer_bgfdy_1')) {
+        } else if (document.querySelector('._resultContainer_bgfdy_1') && isFinish) {
             const playBtn = document.querySelector('#root button._button_uyw8r_1._purple_uyw8r_31._textUppercase_uyw8r_28');
             const finishCoinsText = document.querySelector('#root ._reward_bgfdy_17 > span')?.innerText || '';
             const isVictory = !finishCoinsText.includes('-');
@@ -22,15 +22,14 @@ const checkState = async () => {
             stats.totalCoins += parseInt(finishCoinsText.replace(/[^\d-]/g, ''), 10);
             console.group('Game Result'); console.log(`${consoleStyles.prefix}${isVictory ? '🎉 Victory' : '💀 Defeat'} (${finishCoinsText})`, isVictory ? consoleStyles.green : consoleStyles.red);
             console.log(`${consoleStyles.prefix}[${stats.victories} W | ${stats.defeats} L | ${stats.totalCoins >= 0 ? '+' : ''}${stats.totalCoins.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}]`, consoleStyles.white); console.groupEnd();
-            await sleep(5000); if (playBtn) playBtn.click();
-            return await sleep(1500), await checkState();
+            if (playBtn) return (isFinish = false, await sleep(5000), playBtn.click(), await sleep(1000), await checkState());
         }
     } catch (e) { return await sleep(1000), await checkState(); }
 };
 
 const playBattle = async () => {
     const clickableArea = document.querySelector('.clickableArea');
-    if (clickableArea) { clickableArea.click() } else if (document.querySelectorAll('._card_n90wq_1').length) { document.querySelectorAll('._card_n90wq_1')[Math.floor(Math.random() * 4)].click() } else { if (!document.querySelector('._battle_zmmvw_1')) return isBattle = false, await checkState() }
+    if (clickableArea) { clickableArea.click() } else if (document.querySelectorAll('._card_n90wq_1').length) { document.querySelectorAll('._card_n90wq_1')[Math.floor(Math.random() * 4)].click() } else { if (!document.querySelector('._battle_zmmvw_1')) return isBattle = false, isFinish = true, await checkState() }
     return await sleep(95), await playBattle();
 };
 
